2b4b47a9a6054c27004e7f22c76f8167d0867a30
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-03-28 00:20:37 +0200

    Started implementing logic for adding a note

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import NoteModal from "../components/NoteModal";
+import axios from "axios";
 
 const Home = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const addNote = async (title, description) => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/note/add", {
+        title,
+        description,
+      });
+      if (response.data.success) {
+        closeModal();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <button
+        onClick={() => setModalOpen(true)}
+        className=" fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full"
+      >
+        +
+      </button>
+      {isModalOpen && <NoteModal closeModal={closeModal} addNote={addNote} />}
     </div>
   );
 };
