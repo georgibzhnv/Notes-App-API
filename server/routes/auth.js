0299af8c012314d339d91e7874a2519869b23cbe
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-03-27 22:35:17 +0200

    Finished register logic

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,30 @@
 import express, { response } from 'express'
+import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
 router.post('/register',async(req,resp)=>{
+    try{
+        const{name,email,password}=req.body;
+        const user = await User.findOne({email})
+        if(user){
+            return resp.status(401).json({succes:false,message:"User already exist"})
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            name,email,password:hashPassword
+        })
+
+        await newUser.save()
+
+        return resp.status(200).json({succes:true, message:"Account Created Successfully"})
+    }catch(error){
+        console.log(error.message)
+        return resp.status(500).json({succes:false, message:"Error in adding user "})
+    }
 
 })
 
