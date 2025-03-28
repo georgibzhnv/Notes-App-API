0763ef02c2947f70ecb6df2db3b551bdecbb58b9
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-03-28 10:29:16 +0200

    Visualized notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import NoteModal from "../components/NoteModal";
 import axios from "axios";
+import NoteCard from "../components/NoteCard";
 
 const Home = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/api/note");
+      setNotes(data.notes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const closeModal = () => {
     setModalOpen(false);
@@ -25,6 +40,7 @@ const Home = () => {
         }
       );
       if (response.data.success) {
+        fetchNotes();
         closeModal();
       }
     } catch (error) {
@@ -35,6 +51,11 @@ const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <div className="px-8 pt-4 grid grid-cols-1 md:grid-cols-3 gap-6">
+        {notes.map((note) => (
+          <NoteCard note={note} />
+        ))}
+      </div>
       <button
         onClick={() => setModalOpen(true)}
         className=" fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full"
