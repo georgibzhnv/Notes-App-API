aab2bf02877204f72368b49f31ccd67ed7209eec
georgibzhnv <gbozhinov@tu-sofia.bg>
2025-03-28 11:13:57 +0200

    Added login for searching a note

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,24 @@ import NoteCard from "../components/NoteCard";
 
 const Home = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [filteredNotes, setFilteredNote] = useState(false);
   const [notes, setNotes] = useState([]);
   const [currentNote, setCurrentNote] = useState(null);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     fetchNotes();
   }, []);
+
+  useEffect(() => {
+    setFilteredNote(
+      notes.filter(
+        (note) =>
+          note.title.toLowerCase().includes(query.toLowerCase()) ||
+          note.description.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }, [query, notes]);
 
   const fetchNotes = async () => {
     try {
@@ -97,11 +109,15 @@ const Home = () => {
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar setQuery={setQuery} />
       <div className="px-8 pt-4 grid grid-cols-1 md:grid-cols-3 gap-6">
-        {notes.map((note) => (
-          <NoteCard note={note} onEdit={onEdit} deleteNote={deleteNote} />
-        ))}
+        {filteredNotes.length > 0 ? (
+          filteredNotes.map((note) => (
+            <NoteCard note={note} onEdit={onEdit} deleteNote={deleteNote} />
+          ))
+        ) : (
+          <p>No notes</p>
+        )}
       </div>
       <button
         onClick={() => setModalOpen(true)}
